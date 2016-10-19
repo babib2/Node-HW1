6de9ac8988ef323006e45ab3790a8151ea899aa2
babib2 <al-kogol-ex@mail.ru>
2016-10-19 22:15:48 +0400

    Remove

--- Task3.js
+++ Task3.js
@@ -1,10 +1,7 @@
 var colors = require('colors');
 console.log('hello'.green);
-var AudioContext = require('web-audio-api').AudioContext
-  , context = new AudioContext
 
-context.outStream = process.stdout;
 
 console.log('buy'.underline.red);
 var colors = require('colors/safe');
-console.log(colors.rainbow('онвелс мер?')); 
+console.log(colors.rainbow('buy?')); 

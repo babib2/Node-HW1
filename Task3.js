d01d296559f758024bfa608f5ac6fb63151020cb
babib2 <al-kogol-ex@mail.ru>
2016-10-19 18:16:37 +0400

    Create project

---
+++ Task3.js
@@ -0,0 +1,10 @@
+var colors = require('colors');
+console.log('hello'.green);
+var AudioContext = require('web-audio-api').AudioContext
+  , context = new AudioContext
+
+context.outStream = process.stdout;
+
+console.log('buy'.underline.red);
+var colors = require('colors/safe');
+console.log(colors.rainbow('онвелс мер?')); 
